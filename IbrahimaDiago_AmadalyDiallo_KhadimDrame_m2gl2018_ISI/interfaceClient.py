78d3eb1a900b01e92967ede08a4cf606063a53f6
khazmovic <khaazmovic@github.com>
2018-02-17 00:06:28 +0000

    interface client

---
+++ IbrahimaDiago_AmadalyDiallo_KhadimDrame_m2gl2018_ISI/interfaceClient.py
@@ -0,0 +1,28 @@
+import os
+
+print("_________________Menu__________________")
+print("1 : Gestion des employers ")
+print("2 : Gestion des services ")
+
+
+if(listemp ==1):
+       listemp=(input(" listes des employers"))
+       listemp=(input(" Ajouter employers"))
+       listemp=(input(" Suprimer employers"))
+       listemp=(input(" Modifier employers"))
+else :
+        print("erreur")
+
+
+listserv=input("2:Liste des services")
+ajoutemp=input("3:Ajouter un employers");
+matricule= (input("Donner votre matricule : "))
+print ("Matricule: " ,matricule, ".")
+nom= (input("Donner votre nom : "))
+print("Nom: " ,nom, ".")
+prenom= (input("Donner votre prenom : "))
+print("Prenom: " ,prenom, ".");
+adresse= (input("Donner votre adresse : "))
+print("Adresse: " ,adresse, ".");
+email= (input("Donner votre email : "))
+print("Email: " ,email, ".");
